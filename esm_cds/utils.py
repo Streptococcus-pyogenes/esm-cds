eb16691f5b0315bd6ada8b32d406759b79e02e22
Streptococcus-pyogenes <hhg02258565@gmail.com>
2026-01-05 06:33:13 -0800

    Add utils skeleton

---
+++ esm_cds/utils.py
@@ -0,0 +1,22 @@
+"""
+Utility functions for ESM-CDS.
+
+This module provides helper functions and utilities used across the ESM-CDS codebase. Implementation is TODO.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+def chunk_sequence(seq: list[Any], chunk_size: int) -> list[list[Any]]:
+    """Chunk a sequence into non-overlapping pieces.
+
+    Args:
+        seq: Sequence to split.
+        chunk_size: Size of each chunk.
+
+    Returns:
+        A list of chunks.
+    """
+    # TODO: Implement chunking utility
+    raise NotImplementedError
