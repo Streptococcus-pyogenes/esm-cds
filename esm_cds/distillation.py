f5a78502907e693d8bf628aab98447bac7516fff
Streptococcus-pyogenes <hhg02258565@gmail.com>
2026-01-05 06:31:44 -0800

    Add distillation skeleton

---
+++ esm_cds/distillation.py
@@ -0,0 +1,19 @@
+"""
+Distillation routines for ESM-CDS.
+
+This module defines functions and classes for distillation from a teacher model to the ESM-CDS student. Implementation is TODO.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+def distill(teacher: Any, student: Any, *args: Any, **kwargs: Any) -> None:
+    """Run distillation from teacher to student.
+
+    Args:
+        teacher: Teacher model.
+        student: Student model.
+    """
+    # TODO: Implement distillation procedure
+    raise NotImplementedError
